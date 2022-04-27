2d18afc4d6f82cef1c89a9f13e71227513450958
sunilkumarchoudari <notifysunilkumar@gmail.com>
2022-04-27 17:37:09 +0530

    changes commited in -nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
-
+import logo from '../../assets/images/storeicon.jpeg';
 import useStyles from './styles';
 
 const Navbar = () => {
@@ -16,8 +16,19 @@ const Navbar = () => {
                         variant="5"
                         className={classes.title}
                         color="inherit">
-                        <strong>Online | Book Store</strong>
+                        <img src={logo} alt="Tech Books App" height="50px" className={classses.image} />
+                        <strong>TECH Books</strong>
                     </Typography>
+                    <div className={classes.grow} />
+                    {location.pathname === '/' && (
+                        <div className={classes.button}>
+                            <IconButton component={Link} t="/cart" aria-label='Show Cart item' color='inherit'>
+                                <Badge badgeContent={totalItems} color="secondary">
+                                    <Shoppingcart />
+                                </Badge>
+                            </IconButton>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
 
@@ -25,4 +36,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Navbar
